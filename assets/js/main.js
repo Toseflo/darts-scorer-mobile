a8dbfa9c2aa8f648f7a437034a523bcba2a89e91
Toseflo <66953870+Toseflo@users.noreply.github.com>
2025-10-26 12:56:52 +0100

    Disable autofill in text input (for passwords, addresses, etc.)

--- assets/js/main.js
+++ assets/js/main.js
@@ -115,7 +115,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         playerDiv.className = 'flex items-center space-x-2';
         const placeholderText = `Spieler ${playerNumber}`;
         playerDiv.innerHTML = `
-            <input type="text" value="${placeholderText}" class="input-field" data-i18n-placeholder="player_placeholder" placeholder="${placeholderText}">
+            <input type="text" value="${placeholderText}" class="input-field" data-i18n-placeholder="player_placeholder" placeholder="${placeholderText}" autocomplete="off">
             <button class="remove-player-btn">
                 <svg class="h-6 w-6" viewBox="0 0 24 24" stroke="currentColor" style="fill: none;">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
